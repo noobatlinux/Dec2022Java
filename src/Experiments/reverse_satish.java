69d4fe97b1ed77963da703752dcce5beaeaa8252
noobatlinux <nsaiteja03041998@gmail.com>
2022-12-28 12:24:09 -0600

    added "calculator program using inheritance"

--- src/Experiments/reverse_satish.java
+++ src/Experiments/reverse_satish.java
@@ -1,13 +1,13 @@
-package Tests;
+package Experiments;
 
 public class reverse_satish {
         public static void main(String[] args) {
             String originalStr = "Hello";
-            String reversedStr = "";
+            StringBuilder reversedStr = new StringBuilder();
             System.out.println("Original string: " + originalStr);
 
             for (int i = 0; i < originalStr.length(); i++) {
-                reversedStr = originalStr.charAt(i) + reversedStr;
+                reversedStr.insert(0, originalStr.charAt(i));
             }
 
             System.out.println("Reversed string: "+ reversedStr);
